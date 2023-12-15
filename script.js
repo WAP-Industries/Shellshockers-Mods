fcff1104032e038a9ad7c84cad9e87b301c5909a
WAP Industries <141229412+WAP-Industries@users.noreply.github.com>
2023-12-15 13:17:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -53,134 +53,108 @@
 // ==/UserScript==
 
 window.XMLHttpRequest = class extends window.XMLHttpRequest {
+    open(method, url) {
+        if (url.indexOf('shellshock.js') > - 1) 
+            this.isScript = true;
+        return super.open(...arguments);
+    }
 
-	open(method, url) {
+    get response(){
+        if (this.isScript){
+            const code = super.response
 
-		if (url.indexOf('shellshock.js') > - 1) {
+            let babylonVarName,
+                playersVarName,
+                myPlayerVarName,
+                sceneVarName,
+                cullFuncName;
+            
+            try {
+                babylonVarName = /this\.origin=new ([a-zA-Z]+)\.Vector3/.exec(code)[1];
+                playersVarName = /([^,]+)=\[\],[^,]+=\[\],{}/.exec(code)[1];
+                myPlayerVarName = /"fire":document.pointerLockElement&&([^&]+)&&/.exec(code)[1];
+                sceneVarName = /createMapCells\(([^,]+),/.exec(code)[1];
+                cullFuncName = /=([a-zA-Z_$]+)\(this\.mesh,\.[0-9]+\)/.exec(code)[1];
+            } 
+            catch (error) {
+                alert('Script failed to inject. Report the issue to the script developer.\n' + JSON.stringify(getVars(), undefined, 2));
+                return code;
+            }
 
-			this.isScript = true;
+            function getVars(){
+                return {
+                    babylonVarName,
+                    playersVarName,
+                    myPlayerVarName,
+                    playersVarName,
+                    sceneVarName,
+                    cullFuncName
+                };
+            }
+            console.log('%cInjecting code...', 'color: red; background: black; font-size: 2em;', getVars());
 
-		}
+            return code.replace(sceneVarName + '.render()', `
+                    window[ '${onUpdateFuncName}'](${babylonVarName},${playersVarName},${myPlayerVarName}); 
+                    ${sceneVarName}.render()`)
+                .replace(`function ${cullFuncName}`, `
+                    function ${cullFuncName}() {return true;}
+                    function someFunctionWhichWillNeverBeUsedNow`);
+        }
 
-		return super.open(...arguments);
+        return super.response
+    }
+}
 
-	}
-
-	get response() {
-
-		if (this.isScript) {
-
-			const code = super.response;
-
-			let babylonVarName,
-				playersVarName,
-				myPlayerVarName,
-				sceneVarName,
-				cullFuncName;
-
-			try {
-
-				babylonVarName = /this\.origin=new ([a-zA-Z]+)\.Vector3/.exec(code)[1];
-				playersVarName = /([^,]+)=\[\],[^,]+=\[\],{}/.exec(code)[1];
-				myPlayerVarName = /"fire":document.pointerLockElement&&([^&]+)&&/.exec(code)[1];
-				sceneVarName = /createMapCells\(([^,]+),/.exec(code)[1];
-				cullFuncName = /=([a-zA-Z_$]+)\(this\.mesh,\.[0-9]+\)/.exec(code)[1];
-
-			} catch (error) {
-
-				alert('Script failed to inject. Report the issue to the script developer.\n' + JSON.stringify(getVars(), undefined, 2));
-
-				return code;
-
-			}
-
-			function getVars() {
-
-				return {
-					babylonVarName,
-					playersVarName,
-					myPlayerVarName,
-					playersVarName,
-					sceneVarName,
-					cullFuncName
-				};
-
-			}
-
-			console.log('%cInjecting code...', 'color: red; background: black; font-size: 2em;', getVars());
-
-			return code.replace(sceneVarName + '.render()', `
-
-					window[ '${onUpdateFuncName}' ](
-						${babylonVarName},
-						${playersVarName},
-						${myPlayerVarName}
-					);
-
-				${sceneVarName}.render()`)
-				.replace(`function ${cullFuncName}`, `
-
-					function ${cullFuncName}() {
-
-						return true;
-
-					}
-
-				function someFunctionWhichWillNeverBeUsedNow` );
-
-		}
-
-		return super.response;
-
-	}
-
-};
+const materials = {}
 
 const onUpdateFuncName = btoa(Math.random().toString(32));
 
 window[onUpdateFuncName] = function (BABYLON, players, myPlayer) {
-	if (!myPlayer) return
-
-	try {
-		for (let i = 0; i < players.length; i++) {
-			const player = players[i]
-			if (!player || player === myPlayer) continue
-
-			if (!player.modded) {
-				player.actor.bodyMesh.setEnabled(false)
-
-				function createplane(image) {
-					const material = new BABYLON.StandardMaterial("", player.actor.scene)
-					material.emissiveColor = new BABYLON.Color3.White()
-					material.specularColor = new BABYLON.Color3(0, 0, 0);
-					material.diffuseTexture = new BABYLON.Texture(image, player.actor.scene)
-					material.diffuseTexture.hasAlpha = true
-					material.useAlphaFromDiffuseTexture = true
-
-					const plane = BABYLON.MeshBuilder.CreatePlane("", {
-						width: 0.5,
-						height: 0.75,
-						sideOrientation: BABYLON.Mesh.DOUBLESIDE
-					})
-					plane.material = material
-					plane.position.y = 0.4
-					plane.parent = player.actor.mesh
-					return plane
-				}
-
-				const p1 = createplane("https://i.ibb.co/vkmzKdk/eggcheong-front.png")
-				const p2 = createplane("https://i.ibb.co/bmYRmrr/eggcheong-back.png")
-				p2.position.z = -0.01
-
-				player.modded = true
-			}
-		}
-
-		// if this doesnt print to the console something fucked up somewhere
-		console.log("success")
-	}
-	catch (err) {
-		console.log(err)
-	}
-
+    if (!myPlayer) return
+    
+    try {
+        for (const player of players) {
+            if (!player || player === myPlayer) continue
+            
+            if (!player.modded) {
+                player.actor.bodyMesh.setEnabled(false)
+                
+                function create_plane(image) {
+                    if (!materials[image]){
+                        materials[image] = function(){
+                            const m = new BABYLON.StandardMaterial("", player.actor.scene)
+                            m.emissiveColor = new BABYLON.Color3.White()
+                            m.specularColor = new BABYLON.Color3(0, 0, 0);
+                            m.diffuseTexture = new BABYLON.Texture(image, player.actor.scene)
+                            m.diffuseTexture.hasAlpha = true
+                            m.useAlphaFromDiffuseTexture = true
+                            return m 
+                        }()
+                    }
+                    
+                    const plane = BABYLON.MeshBuilder.CreatePlane("", {	
+                        width: 0.5,
+                        height: 0.75,
+                        sideOrientation: BABYLON.Mesh.DOUBLESIDE
+                    })
+                    plane.material = materials[image]
+                    plane.position.y = 0.4
+                    plane.parent = player.actor.mesh
+                    return plane
+                }
+                
+                const p1 = create_plane("https://i.ibb.co/vkmzKdk/eggcheong-front.png")
+                const p2 = create_plane("https://i.ibb.co/bmYRmrr/eggcheong-back.png")
+                p2.position.z = -0.01
+                
+                player.modded = true
+            }
+        }
+        
+        // if this doesnt print to the console something fucked up somewhere
+        console.log("mod is running")
+    }
+    catch (err) {
+        console.log(err)
+    }
 }
